86c6638a0cc30d5bd1333c073f0c9af85b86d683
Taewon Kim <taewonkim2001@outlook.com>
2022-11-03 19:01:52 -0700

    edits

--- src/main.py
+++ src/main.py
@@ -11,19 +11,25 @@ from systems.double_pendulum import DoublePendulum, DPParams
 from systems.reaction_wheel import ReactionWheel, RWParams
 from systems.three_link_reaction_wheel import TLRW, TLRWParams
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulate a robotic system.")
     parser.add_argument("system", type=str, help="The robotic system to simulate.")
-    parser.add_argument("--q0", "-q", type=float, nargs="+", required=True, help="Initial joint angles and velocities.")
-    parser.add_argument("--duration", "-D", type=int, default=5, help="Duration of the animation.")
-    parser.add_argument("--fps", "-F", type=int, default=30, help="Frames per second of the animation.")
-    parser.add_argument("--speed", "-S", type=float, default=1.0, help="Speed of the animation.")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Print verbose output.")
+    parser.add_argument("--q0", type=float, nargs="+", required=True, help="Initial joint angles and velocities.")
+    parser.add_argument("--duration", "-d", type=int, default=5, help="Duration of the animation.")
+    parser.add_argument("--fps", "-f", type=int, default=30, help="Frames per second of the animation.")
+    parser.add_argument("--speed", "-s", type=float, default=1.0, help="Speed of the animation.")
     parser.add_argument("--save", action="store_true", help="Save the animation.")
     parser.add_argument("--no-show", action="store_true", help="Do not show the animation.")
 
     args = parser.parse_args()
 
-    print("Solving the system...")
+    def vprint(print_str):
+        if args.verbose:
+            print(print_str)
+
+    vprint("Solving the system...")
     start_solve = perf_counter()
 
     try:
@@ -42,16 +48,16 @@ if __name__ == "__main__":
         elif args.system.lower() == "tlrw":
             system = TLRW(TLRWParams())
         else:
-            print("Please specify a system to simulate.")
+            vprint("Please specify a system to simulate.")
             exit(1)
     except Exception as e:
         end_solve = perf_counter()
-        print(f"System solving failed after {end_solve - start_solve:.3f} seconds.\n")
+        vprint(f"System solving failed after {end_solve - start_solve:.3f} seconds.\n")
         raise e
 
     end_solve = perf_counter()
 
-    print(f"System solved in {end_solve - start_solve:.3f} seconds.\n")
+    vprint(f"System solved in {end_solve - start_solve:.3f} seconds.\n")
 
     timestamp = datetime.now().isoformat(sep='T', timespec='seconds')
     with open(f"cache/{system.__class__.__name__}_{timestamp}", "wb") as outfile:
@@ -59,7 +65,7 @@ if __name__ == "__main__":
         pickle.dump(system, outfile)
         end_serialize = perf_counter()
 
-    print(f"System serialized in {end_serialize - start_serialize:.3f} seconds.\n")
+    vprint(f"System serialized in {end_serialize - start_serialize:.3f} seconds.\n")
 
     sim = Simulator(
         system=system,
@@ -73,12 +79,12 @@ if __name__ == "__main__":
         sim.run(np.array(args.q0))
     except Exception as e:
         end_sim = perf_counter()
-        print(f"Simulation failed after {end_sim - start_sim:.3f} seconds.\n")
+        vprint(f"Simulation failed after {end_sim - start_sim:.3f} seconds.\n")
         raise e
 
     end_sim = perf_counter()
 
-    print(f"Simulation time: {end_sim - start_sim:.3f} sec\n")
+    vprint(f"Simulation time: {end_sim - start_sim:.3f} sec\n")
 
     ani = Animator(
         links=sim.links,
@@ -90,31 +96,31 @@ if __name__ == "__main__":
 
     )
 
-    print("Animating...")
+    vprint("Animating...")
     start_ani = perf_counter()
 
     try:
         ani.run()
     except Exception as e:
         end_ani = perf_counter()
-        print(f"Animation failed after {end_ani - start_ani:.3f} seconds.\n")
+        vprint(f"Animation failed after {end_ani - start_ani:.3f} seconds.\n")
         raise e
 
     end_ani = perf_counter()
 
-    print(f"Animation time: {end_ani - start_ani:.3f} sec\n")
+    vprint(f"Animation time: {end_ani - start_ani:.3f} sec\n")
 
     if args.save:
-        print("Saving...")
+        vprint("Saving...")
         start_save = perf_counter()
 
         try:
             ani.save(sim.system.__class__.__name__)
         except Exception as e:
             end_save = perf_counter()
-            print(f"Save failed after {end_save - start_save:.3f} seconds.\n")
+            vprint(f"Save failed after {end_save - start_save:.3f} seconds.\n")
             raise e
 
         end_save = perf_counter()
 
-        print(f"Save time: {end_save - start_save:.3f} sec\n")
+        vprint(f"Save time: {end_save - start_save:.3f} sec\n")
